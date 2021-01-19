f501408564b22cc069c086560433a24e0de167cc
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-01-19 12:29:51 +0100

    Documentation for Layout.js(more) + svg map

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,17 @@
 import React from "react";
+
 import "./DrawerToggle.css";
 
+/**
+ * @alias DrawerToggle
+ * @memberOf Layout
+ * @param props
+ * @return {JSX.Element}
+ * @description The functional component DrawerToggle return the element DrawerToggle,
+ * that is the element responsible to open and close the sideDrawer.
+ * The onClick properties is connected with Layout through the component SideDrawer
+ * and there, it change the boolean value of the state 'showSideDrawer' by invoking the method toggleSideDrawerHandler.
+ */
 const drawerToggle = (props) => (
     <div className="DrawerToggle" onClick={props.clicked}>
         <div />
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,6 +3,14 @@ import React from "react";
 import  "./Toolbar.css"
 import  DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
 
+/**
+ * @alias Toolbar
+ * @memberOf Layout
+ * @param props
+ * @return {JSX.Element}
+ * @description The functional component Toolbar return the header of the page.
+ * It contains the DrawerToggle, Logo and the NavigationItems.
+ */
 const toolbar = (props) => (
     <header className="Toolbar">
         <DrawerToggle clicked={props.toggleSideDrawer}/>
